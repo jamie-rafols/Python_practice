e8198db38012587fffea74b2feb515803cdd7c74
ainanax <catyjamei@gmail.com>
2026-02-12 19:13:33 +0800

    string

---
+++ Practice loop program/07.2whilereverse.py
@@ -0,0 +1,3 @@
+num = input("Enter a number: ")
+
+print(int(str(num)[::-1]))
---
+++ String/3_slicing.py
@@ -0,0 +1,14 @@
+name = "Harry0123456789"
+
+# print(name[0:2]) #goes from 0 to 1
+
+# print(name[2:-1]) # same as name[2:4]
+
+# print(name[0:10:n]) #skip n-1 characters
+
+print(name[0:10:1]) #skip 0 character
+print(name[0:10:2]) #skip 1 character
+
+
+print(name[:4]) # replace the first emplty number with 0
+print(name[1:]) # replace the second empley number with lenght # name [1:15]

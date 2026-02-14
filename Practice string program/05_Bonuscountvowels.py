3e8921d5d865c885991019dddf0568f8aeb9a342
ainanax <catyjamei@gmail.com>
2026-02-14 21:38:25 +0800

    string

--- Practice string program/05_Bonuscountvowels.py
+++ Practice string program/05_Bonuscountvowels.py
@@ -2,9 +2,10 @@ sen = "This is a sentencewe"
 sum = 0
 vowels = ['a', 'e','i','o','u']
 
-for char in sen:
+for char in sen.lower():
    # print(char)
     if (char in vowels):
         sum+=1
         
 print(f"There are {sum} vowels in this sentence")
+print(vowels)
---
+++ Practice string program/06_Palindrome.py
@@ -0,0 +1,6 @@
+string1="jamie"
+
+if (string1==string1[::-1]):
+    print("The string is a palindrome")
+else:
+    print("This string is not a palindrome")

dadf4bc1fdd6e47eb79dde9996abc56db3987338
ainanax <catyjamei@gmail.com>
2026-02-16 18:28:18 +0800

    function

---
+++ 03_Function/02_functionargument.py
@@ -0,0 +1,8 @@
+def add(a,b,plus=0): #a and b are parameters
+    return a + b + plus
+
+print(add(3,5,2)) #3 and 5 are arguments
+
+
+c1 = add(b=5,a=3) #keyword arguments
+print(c1)
---
+++ 03_Function/03_lambda.py
@@ -0,0 +1,21 @@
+square = lambda x: x*x 
+'''
+as good as writing 
+
+def square(x):
+    return x*x
+
+'''
+
+sum = lambda x ,y: x+y
+
+'''
+As giid as writing 
+def sum (x,y):
+    return x + y
+
+'''
+
+
+print(square(3))
+print(sum(3,62))
